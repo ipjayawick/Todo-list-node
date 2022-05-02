472cd13b5247d57acd688acd47098b9a5e468e2d
IP Jayawickrama <ipjayawick@gmail.com>
2022-05-02 17:05:42 +0530

    working build

--- dist/script.js
+++ dist/script.js
@@ -9,31 +9,37 @@ add_button.addEventListener("click", e => {
     input_text.focus()
 });
 
-input_text.addEventListener("keyup",e=>{
-    if(e.key==='Enter')add_todo();
+input_text.addEventListener("keyup", e => {
+    if (e.key === 'Enter') add_todo();
 })
 
-function add_del_action(){
+function add_del_action() {
     del_buttons = document.querySelectorAll('.del-btn');
     del_buttons.forEach(btn => {
-        btn.addEventListener("click",e=>{
-            remove_todo(e,btn)
+        btn.addEventListener("click", e => {
+            remove_todo(e, btn)
             input_text.focus()
         })
     });
 }
 
-function remove_todo(e,btn){
-    btn.parentElement.outerHTML=''
+function remove_todo(e, btn) {
+    btn.parentElement.parentElement.outerHTML=''
 }
 
-function add_todo(){
-    if (input_text.value==='') return;
+function add_todo() {
+    if (input_text.value === '') return;
     todo_list.innerHTML +=
-    `<div class="card item-card">
-    <p>${input_text.value}</p>
-    <button class="btn del-btn"><i class="fa fa-trash" aria-hidden="true"></i></button>
-    </div>`   
-    input_text.value=''
+    `<li class="list-group-item py-0">
+        <div class="d-flex my-0 py-0 align-items-center">
+            <p class="lead m-0">${ input_text.value }</p>
+            <button class="btn ms-auto del-btn">
+                <h4 class="m-0"><i class="fa fa-trash"></i></h4>
+            </button>
+        </div>
+    </li>`
+
+    
+    input_text.value = ''
     add_del_action()
 }
